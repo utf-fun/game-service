dd44bd238fe292eaefc233efea7b6469a67a83e8
utfunderscore <83981186+utfunderscore@users.noreply.github.com>
2025-06-03 23:25:36 +0100

    feat: add initial Gradle build configuration with gRPC and dependencies

---
+++ server/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "org.readutf.game"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    implementation("org.tinylog:tinylog-api:2.7.0")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
+    implementation("org.tinylog:slf4j-tinylog:2.7.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
